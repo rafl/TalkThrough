6e33af4bcf3059b8ce1a742f76189498f4cc56d1
Florian Ragwitz <rafl@debian.org>
2011-07-30 19:15:52 +0200

    Initial SDK-generated application

---
+++ src/org/perldition/talkthrough/TalkThroughSettings.java
@@ -0,0 +1,15 @@
+package org.perldition.talkthrough;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TalkThroughSettings extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
